f4f951eb586bb68a0f3c7f13956a6ee5e1131708
Dakota Maulding <dakota.maulding+gitlab@gmail.com>
2021-01-30 18:52:29 -0600

    Added publish plugin and java docs

--- src/main/java/org/yellolab/Keyring.java
+++ src/main/java/org/yellolab/Keyring.java
@@ -6,28 +6,29 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class Keyring {
-	static {
-		String nativeLibrary = "libkeyring_jni";
-		File temp;
-		try (InputStream in = Keyring.class.getClassLoader().getResourceAsStream(nativeLibrary)) {
-			byte[] buffer = new byte[1024];
-			int read = -1;
-			temp = File.createTempFile(nativeLibrary, "");
-			try (FileOutputStream fos = new FileOutputStream(temp)) {
-				
-				while ((read = in.read(buffer)) != -1) {
-					fos.write(buffer, 0, read);
-				}
-			}
-		} catch (IOException e) {
-			throw new RuntimeException("Could not generate DLL for the Keyring class", e);
-		}
-		System.load(temp.getAbsolutePath());
-	}
-	
-	public static native String getSecret(String domain, String userName) throws Exception;
-	
-	public static native String setSecret(String domain, String username, String secret) throws Exception;
-	
-	public static native String deleteSecret(String domaind, String username) throws Exception;
+  static {
+    String nativeLibrary = "libkeyring_jni";
+    File temp;
+    try (InputStream in = Keyring.class.getClassLoader().getResourceAsStream(nativeLibrary)) {
+      byte[] buffer = new byte[1024];
+      int read = -1;
+      temp = File.createTempFile(nativeLibrary, "");
+      try (FileOutputStream fos = new FileOutputStream(temp)) {
+
+        while ((read = in.read(buffer)) != -1) {
+          fos.write(buffer, 0, read);
+        }
+      }
+    } catch (IOException e) {
+      throw new RuntimeException("Could not generate DLL for the Keyring class", e);
+    }
+    System.load(temp.getAbsolutePath());
+  }
+
+  public static native String getSecret(String domain, String userName) throws Exception;
+
+  public static native String setSecret(String domain, String username, String secret)
+      throws Exception;
+
+  public static native String deleteSecret(String domaind, String username) throws Exception;
 }
--- src/main/java/org/yellolab/exceptions/KeyringException.java
+++ src/main/java/org/yellolab/exceptions/KeyringException.java
@@ -1,47 +1,47 @@
 package org.yellolab.exceptions;
 
-/***
- * Exceptions used in the rust jni library.
- * Mapped from the rust keyring package enum keyring::KeyringError
+/**
+ * * Exceptions used in the rust jni library. Mapped from the rust keyring package enum
+ * keyring::KeyringError
  */
 public class KeyringException extends RuntimeException {
-	public KeyringException(String exception) {
-		super(exception);
-	}
+  public KeyringException(String exception) {
+    super(exception);
+  }
 }
 
 class NoBackendFoundException extends KeyringException {
-	NoBackendFoundException(String exception) {
-		super(exception);
-	}
+  NoBackendFoundException(String exception) {
+    super(exception);
+  }
 }
 
 class NoPasswordFoundException extends KeyringException {
-	NoPasswordFoundException(String exception) {
-		super(exception);
-	}
+  NoPasswordFoundException(String exception) {
+    super(exception);
+  }
 }
 
 class ParseException extends KeyringException {
-	ParseException(String exception) {
-		super(exception);
-	}
+  ParseException(String exception) {
+    super(exception);
+  }
 }
 
 class MacOsKeychainException extends KeyringException {
-	MacOsKeychainException(String exception) {
-		super(exception);
-	}
+  MacOsKeychainException(String exception) {
+    super(exception);
+  }
 }
 
 class WindowsVaultException extends KeyringException {
-	WindowsVaultException(String exception) {
-		super(exception);
-	}
+  WindowsVaultException(String exception) {
+    super(exception);
+  }
 }
 
 class SecretServiceException extends KeyringException {
-	SecretServiceException(String exception) {
-		super(exception);
-	}
+  SecretServiceException(String exception) {
+    super(exception);
+  }
 }
